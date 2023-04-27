7c894cccccab4fbfa8e00f2154b7f70d4ce27a52
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-04-27 07:49:24 +0300

    halfway assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({onAdd}) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -15,12 +15,35 @@ function QuestionForm(props) {
       ...formData,
       [event.target.name]: event.target.value,
     });
+    /*
+    fetch(`http://localhost:4000/questions/:id`, {
+      method: " PATCH",
+      headers: {
+        "Content-Type" :"application/json"
+      },
+      body: {"correctIndex": event.target.value}
+    })
+    */
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    // console.log(formData);
+    
+    fetch("http://localhost:4000/questions", {
+      method: " POST",
+      headers: {
+        "Content-Type" :"application/json"
+      },
+      body: JSON.stringify(
+        {"prompt" : formData.prompt,
+         "answers" : [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+        "correctIndex" : formData.correctIndex}
+      )
+    })
+    
   }
+  
 
   return (
     <section>
